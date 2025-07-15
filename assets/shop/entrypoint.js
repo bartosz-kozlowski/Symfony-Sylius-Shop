b392cdd7d343805c4755dbc16cd7c90d420a5664
Bartosz Kozłowski <barkoz.2003@gmail.com>
2025-07-15 15:30:49 +0200

    Zmiana widoku w koszyku w panelu uzytkownika

--- assets/shop/entrypoint.js
+++ assets/shop/entrypoint.js
@@ -1,5 +1,4 @@
 import '../../vendor/sylius/refund-plugin/assets/entrypoint';
-import '@vendor/sylius/mollie-plugin/assets/admin/entrypoint';
 import './bootstrap.js';
 import './configurator.js';
 
