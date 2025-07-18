a0b07ea8c7787260a0f9e85b00b9df2126cb1228
Bartosz Kozłowski <barkoz.2003@gmail.com>
2025-07-18 14:35:01 +0200

    Poprawki frontendowe

--- assets/admin/entrypoint.js
+++ assets/admin/entrypoint.js
@@ -2,6 +2,6 @@ import '../../vendor/sylius/refund-plugin/assets/entrypoint';
 import '@vendor/sylius/mollie-plugin/assets/admin/entrypoint';
 import './bootstrap.js';
 import './overview.js';
-
+import './styles/overrides.css'
 // In this file you can import assets like images or stylesheets
 console.log('siemano! Edit me in assets/admin/entrypoint.js!!!!');
