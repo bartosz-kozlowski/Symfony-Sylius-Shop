fe088236fa8a7631ce0c1226083629f9837691df
Bartosz Kozłowski <barkoz.2003@gmail.com>
2025-07-18 13:22:16 +0200

    poprawki frontendowe

--- assets/shop/entrypoint.js
+++ assets/shop/entrypoint.js
@@ -1,4 +1,5 @@
 import '../../vendor/sylius/refund-plugin/assets/entrypoint';
 import './bootstrap.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 console.log('jestem sobie w shop entrypoint');
