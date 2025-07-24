3c126522a54edc9661dcd1d1a202f05da81c5cb3
Bartosz Kozłowski <barkoz.2003@gmail.com>
2025-07-24 08:51:21 +0200

    checkout zmiany frontendowe

--- assets/shop/entrypoint.js
+++ assets/shop/entrypoint.js
@@ -2,7 +2,6 @@ import '../../vendor/sylius/refund-plugin/assets/entrypoint';
 import './bootstrap.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './tracker.js'
-// import '@hotwired/turbo';
 
 console.log('jestem sobie w shop entrypoint');
 console.trace()
